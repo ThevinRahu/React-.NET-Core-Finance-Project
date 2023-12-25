e2a1938e060fd9492113b989d74c3ecb084c0132
ThevinRahu <thevinvr@gmail.com>
2023-12-25 11:49:17 +0530

    update stocks

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using api.Data;
+using api.Dtos.Stock;
+using api.DTOs.Stock;
 using api.Mappers;
 using Microsoft.AspNetCore.Mvc;
 
@@ -39,5 +41,38 @@ namespace api.Controllers
 
             return Ok(stock.ToStockDto());
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CreateStockRequestDto stockDto)
+        {
+            var stockModel = stockDto.ToStockFromCreateDTO();
+            // await _stockRepo.CreateAsync(stockModel);
+             _context.Stocks.Add(stockModel);
+             _context.SaveChanges();
+            return CreatedAtAction(nameof(GetById), new { id = stockModel.Id }, stockModel.ToStockDto());
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public async Task<IActionResult> Update([FromRoute] int id, [FromBody] UpdateStockRequestDto updateDto)
+        {
+            var stock =  _context.Stocks.Find(id);
+            // var stockModel = await _stockRepo.UpdateAsync(id, updateDto);
+
+            if (stock == null)
+            {
+                return NotFound();
+            }else{
+                stock.Symbol = updateDto.Symbol;
+                stock.CompanyName = updateDto.CompanyName;
+                stock.Purchase = updateDto.Purchase;
+                stock.LastDiv = updateDto.LastDiv;
+                stock.Industry = updateDto.Industry;
+                stock.MarketCap = updateDto.MarketCap;
+                _context.SaveChanges();
+                return Ok(stock.ToStockDto());
+            }
+        }
+
     }
 }
