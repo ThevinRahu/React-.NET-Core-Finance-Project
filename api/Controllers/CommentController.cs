bdc89831d7e72d37b621090f434d121c679fc03b
ThevinRahu <thevinvr@gmail.com>
2024-01-06 19:57:11 +1300

    comments work

---
+++ api/Controllers/CommentController.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Interfaces;
+using api.Mappers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [Route("api/comment")]
+    [ApiController]
+    public class CommentController: ControllerBase
+    {
+        private readonly ICommentRepository _commentRepo;
+        private readonly IStockRepository _stockRepo;
+        public CommentController(ICommentRepository commentRepo, IStockRepository stockRepo)
+        {
+            _commentRepo = commentRepo;
+            _stockRepo = stockRepo;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var comments = await _commentRepo.GetAllAsync();
+
+            var commentDto = comments.Select(s => s.ToCommentDto());
+
+            return Ok(commentDto);
+        }
+
+    }
+}
