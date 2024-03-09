6b0101748e67621ca4b6cba1825dd02e1aaf5c66
ThevinRahu <thevinvr@gmail.com>
2024-03-09 13:58:32 +1300

    data seeding

--- api/Controllers/PortfolioController.cs
+++ api/Controllers/PortfolioController.cs
@@ -18,14 +18,14 @@ namespace api.Controllers
         private readonly UserManager<AppUser> _userManager;
         private readonly IStockRepository _stockRepo;
         private readonly IPortfolioRepository _portfolioRepo;
-        //private readonly IFMPService _fmpService;
+        private readonly IFMPService _fmpService;
         public PortfolioController(UserManager<AppUser> userManager,
-        IStockRepository stockRepo, IPortfolioRepository portfolioRepository)
+        IStockRepository stockRepo, IPortfolioRepository portfolioRepository,IFMPService fmpService)
         {
             _userManager = userManager;
             _stockRepo = stockRepo;
             _portfolioRepo = portfolioRepository;
-            // _fmpService = fmpService;
+            _fmpService = fmpService;
         }
 
         [HttpGet]
@@ -46,18 +46,18 @@ namespace api.Controllers
             var appUser = await _userManager.FindByNameAsync(username);
             var stock = await _stockRepo.GetBySymbolAsync(symbol);
 
-            // if (stock == null)
-            // {
-            //     stock = await _fmpService.FindStockBySymbolAsync(symbol);
-            //     if (stock == null)
-            //     {
-            //         return BadRequest("Stock does not exists");
-            //     }
-            //     else
-            //     {
-            //         await _stockRepo.CreateAsync(stock);
-            //     }
-            // }
+            if (stock == null)
+            {
+                stock = await _fmpService.FindStockBySymbolAsync(symbol);
+                if (stock == null)
+                {
+                    return BadRequest("Stock does not exists");
+                }
+                else
+                {
+                    await _stockRepo.CreateAsync(stock);
+                }
+            }
 
             if (stock == null) return BadRequest("Stock not found");
 
