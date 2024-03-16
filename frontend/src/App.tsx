9238de1b899897b9d005ab13bc007a72c46093b4
ThevinRahu <thevinvr@gmail.com>
2024-03-17 11:57:17 +1300

    react services

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import { Outlet } from 'react-router';
 import './App.css';
 import Navbar from './Components/NavBar/NavBar';
+import { ToastContainer } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css"
 
 function App() {
   
@@ -8,6 +10,7 @@ function App() {
     <>
     <Navbar/>
     <Outlet/>
+    <ToastContainer/>
     </>
   );
 }
